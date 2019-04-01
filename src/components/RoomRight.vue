2b767a8f7f0d6c585363ee3f37f576041aaabb53
ilya shum <ilya.shum@conversion.com>
2019-04-01 17:46:09 +0100

    progress

---
+++ src/components/RoomRight.vue
@@ -0,0 +1,26 @@
+<template>
+  <div><span>{{ todo.name }}</span><span> {{ todo.when }}</span></div>  
+</template>
+
+<script>
+export default {
+  name: 'RoomRight',
+  props: {
+    todo: Object
+  },
+  data: function() {
+    // var eventR='a';
+    return {
+      // eventR: roomR[0].name
+    }
+    
+  }  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
